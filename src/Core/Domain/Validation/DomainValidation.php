ae4c1587c7b3ff2b31fa40f5a02e40cfd13093e1
eskokado <eskokado@gmail.com>
2025-03-08 17:21:32 -0300

    fix: str max length when empty throw exception

--- src/Core/Domain/Validation/DomainValidation.php
+++ src/Core/Domain/Validation/DomainValidation.php
@@ -15,14 +15,14 @@ class DomainValidation
 
   public static function strMaxLength(string $value, int $length = 255, string $exceptMessage = null)
   {
-    if (strlen($value) > $length) {
+    if (empty($value) || strlen($value) > $length) {
       throw new EntityValidationException($exceptMessage ?? "The value must not be greater than {$length} characters");
     }
   }
 
   public static function strMinLength(string $value, int $length = 5, string $exceptMessage = null)
   {
-    if (strlen($value) < $length) {
+    if (empty($value) || strlen($value) < $length) {
       throw new EntityValidationException($exceptMessage ?? "The value must be at least {$length} characters");
     }
   }
