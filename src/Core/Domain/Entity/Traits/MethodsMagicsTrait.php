48e89175c04a47f9f035562723df8e51998132f6
eskokado <eskokado@gmail.com>
2025-03-09 17:53:33 -0300

    feat: impl get id

--- src/Core/Domain/Entity/Traits/MethodsMagicsTrait.php
+++ src/Core/Domain/Entity/Traits/MethodsMagicsTrait.php
@@ -6,10 +6,15 @@ trait MethodsMagicsTrait
 {
   public function __get($property)
   {
-    if (isset($this->{$property})) 
+    if (isset($this->{$property}))
       return $this->{$property};
-    
+
     $className = get_class($this);
     throw new Exception("Property {$property} not found in class {$className}");
+  }
+
+  public function id(): string
+  {
+    return (string) $this->id;
   }
 }
