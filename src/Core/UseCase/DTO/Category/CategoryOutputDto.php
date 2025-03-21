48cae9e42618477da229d4ca6019742fc994c1fe
eskokado <eskokado@gmail.com>
2025-03-21 13:13:21 -0300

    fix: rename category output dto

--- src/Core/UseCase/DTO/Category/CategoryOutputDto.php
+++ src/Core/UseCase/DTO/Category/CategoryOutputDto.php
@@ -1,16 +1,16 @@
 <?php
 
- namespace Core\UseCase\DTO\Category;
+namespace Core\UseCase\DTO\Category;
 
- class CategoryCreateOutputDto
- {
+class CategoryOutputDto
+{
   public function __construct(
     public string $id,
     public string $name,
     public string $description = '',
     public bool $is_active = true,
-  )
-  {
-    
+    public string $created_at = '',
+  ) {
+
   }
- }
+}
